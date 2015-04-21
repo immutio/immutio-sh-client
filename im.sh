f7e644c949cb34026eb80ae2987cb6b842120879
Trey Griffith <trey.griffith@gmail.com>
2015-04-21 00:00:21 -0700

    create the initial shell script that reads from a file to upload

---
+++ im.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Usage Info
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-t TYPE] [FILENAME]
+Create an immut.io blob from FILENAME and write the blob URI to standard output.
+With no [FILENAME], read standard input.
+
+    -h        display this help and exit
+    -t TYPE   Use the specified TYPE when uploading
+
+EOF
+}
+
+# Initialize our own variables:
+filename=""
+type=""
+
+OPTIND=1 # Reset is necessary if getopts was used previously in the script.  It is a good idea to make this local in a function.
+while getopts "ht:" opt; do
+    case "$opt" in
+        h)
+            show_help
+            exit 0
+            ;;
+        t)  type=$OPTARG
+            ;;
+        '?')
+            show_help >&2
+            exit 1
+            ;;
+    esac
+done
+shift "$((OPTIND-1))" # Shift off the options and optional --.
+
+filename="$1"
+
+# Read the mimetype from the file, or use text/plain as the default
+if [ -f "$filename" ] && [ "$type" = "" ]
+  then
+    type=$(file --mime-type -b "$filename")
+elif [ "$type" = "" ]
+  then
+    type="text/plain"
+fi
+
+
+# Upload the file and get the resulting relative path
+path=$(curl -sX POST http://immut.io/blobs --upload-file "$filename" -H "Content-Type: $type")
+
+printf 'http://immut.io%s' "$path"
+
+# End of file
